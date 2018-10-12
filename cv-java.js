5dd0eb9e85afd6e28d1377abcb3228353a5d1a36
Marko1976 <43751368+Marko1976@users.noreply.github.com>
2018-10-12 16:18:18 +0200

    Version1

---
+++ cv-java.js
@@ -0,0 +1,38 @@
+function cacher() {
+
+    var x = document.getElementById('mybutton');
+    if (x.style.display === 'none') {
+        x.style.display = '';
+        document.getElementById('btn btn-info').innerHTML = '-';
+    } else {
+        x.style.display = 'none';
+        document.getElementById('btn btn-info').innerHTML = '+';
+
+    }
+}
+
+function cacher1() {
+
+    var x = document.getElementById('mybutton1');
+    if (x.style.display === 'none') {
+        x.style.display = '';
+        document.getElementById('btn btn-info').innerHTML = '-';
+    } else {
+        x.style.display = 'none';
+        document.getElementById('btn btn-info').innerHTML = '+';
+
+    }
+
+}
+function cacher2() {
+
+    var x = document.getElementById('mybutton2');
+    if (x.style.display === 'none') {
+        x.style.display = '';
+        document.getElementById('btn btn-info').innerHTML = '-';
+    } else {
+        x.style.display = 'none';
+        document.getElementById('btn btn-info').innerHTML = '+';
+
+    }
+}
